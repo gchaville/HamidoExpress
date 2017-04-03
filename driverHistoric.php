0acb579e3fc698a1e323021e10bee7ddaa75b635
Mathieu <mathieu.naud@hotmail.com>
2017-04-02 22:25:31 -0400

    bonhomme alumette et enlever les navs

--- driverHistoric.php
+++ driverHistoric.php
@@ -79,26 +79,6 @@ if (!isset($_SESSION['username']) && !isset($_SESSION['pwd']))
         ?>
         </tbody>
     </table>
-
-    <nav aria-label="Page navigation">
-        <ul class="pagination">
-            <li>
-                <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li>
-                <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
     <button type="button" class="btn btn-primary" onclick="location.href='page_member.php'" >Retour</button>
 </section>
 
